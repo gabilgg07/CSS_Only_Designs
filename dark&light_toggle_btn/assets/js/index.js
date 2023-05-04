46a2dedb3a19a3d776513815760fb36b946d0f75
gabilgg07 <gabilgg07@gmail.com>
2023-05-05 00:49:19 +0400

    Modified all designs

--- dark&light_toggle_btn/assets/js/index.js
+++ dark&light_toggle_btn/assets/js/index.js
@@ -2,5 +2,5 @@ const toggle = document.getElementById("toggle");
 
 toggle.addEventListener("click", function (e) {
   toggle.classList.toggle("active");
-  document.body.classList.toggle("active");
+  document.querySelector(".wrapper").classList.toggle("active");
 });
